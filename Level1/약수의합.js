b7ad725c3fe699d2f94548bdfc2e812b736a896b
Scott <sp863429@gmail.com>
2022-01-06 23:04:14 +0900

    level1 : 약수의 합 solution

--- Level1/약수의합.js
+++ Level1/약수의합.js
@@ -9,3 +9,8 @@ function solution(n) {
 
   return answer;
 }
+
+//IDEAL SOLUTION
+// function solution(n, a = 0, b = 0) {
+//   return n <= a / 2 ? b : solution(n, a + 1, (b += n % a ? 0 : a));
+// }
