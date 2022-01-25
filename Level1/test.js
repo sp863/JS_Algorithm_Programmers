83a543162716e6595151ce901a72122f34e7e243
Scott <sp863429@gmail.com>
2022-01-25 21:10:11 +0900

    level1: 예산

--- Level1/test.js
+++ Level1/test.js
@@ -1,27 +1,19 @@
-"use strict";
+const something = {
+  age: 10,
+  speak: function () {
+    console.log(this.age);
+  },
+};
 
-function solution(s) {
-  let numbers = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
-  var answer = s;
+const butler = {
+  age: 20,
+  serve: function (cb) {
+    cb();
+  },
+};
 
-  for (let i = 0; i < numbers.length; i++) {
-    let arr = answer.split(numbers[i]);
-    console.log(arr);
-    answer = arr.join(i);
-  }
-
-  return Number(answer);
-}
-
-solution("one4seveneight");
+// Q: 아래와 같이 실행했을 경우, 콘솔에 출력되는 값이 무엇일까요?
+//    고민해본 후, 주석을 해제하여 정답을 확인해보세요.
+//
+debugger;
+butler.serve(something.speak);
